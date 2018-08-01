8f37abea1dda556080d5eeeebbf8b7b938fcd4fe
Ian Worthington <worthy.vii@gmail.com>
2018-08-01 10:33:26 +0900

    Added readonly parameter

--- projects/ngx-tui-calendar/src/lib/ngx-tui-calendar-defaults.service.ts
+++ projects/ngx-tui-calendar/src/lib/ngx-tui-calendar-defaults.service.ts
@@ -9,7 +9,8 @@ export interface TuiCalendarOptions {
 	template?: Template;
 	month?: MonthOptions;
   week?: WeekOptions;
-  disableDblClick?:boolean
+  disableDblClick?: boolean,
+  isReadOnly? : boolean
 }
 
 export class TuiCalendarDefaults {
@@ -19,6 +20,8 @@ export class TuiCalendarDefaults {
 	useDetailPopup = false;
   scheduleView = true;
   disableDblClick = false;
+  disableDblClick = false;
+  isReadOnly = false;
 
   week: WeekOptions = {
 		daynames: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
